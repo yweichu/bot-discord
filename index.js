ea30f7cb3623628ccf30046a289deb5d770679b0
eth <113138331+yweichu@users.noreply.github.com>
2025-06-30 23:00:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
+  const canaliTarget = ['1380543486054633533', '1380543627180376084'];
+  const botTweetID = '1174332637322674186';
+
   if (
-    message.channel.id === '1380543486054633533' && '1380543627180376084'
-    message.author.id === '1174332637322674186'
+    canaliTarget.includes(message.channel.id) &&
+    message.author.id === botTweetID
   ) {
     message.channel.send('<@&1379823108881645629> nuovo tweet! disabilita ping in <#1379151280240332830> <:kohane_sip:1389349346826260620>');
   }
 });
+
 
 client.login(process.env.TOKEN);
